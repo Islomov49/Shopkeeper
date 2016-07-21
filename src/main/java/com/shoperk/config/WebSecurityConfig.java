8afbe4184fc03bdc4682cc808b23a48c3b2501a1
islomov49 <islomov49@gmail.com>
2016-07-21 19:22:10 +0500

    Rabochiy sostayaniya

--- src/main/java/com/shoperk/config/WebSecurityConfig.java
+++ src/main/java/com/shoperk/config/WebSecurityConfig.java
@@ -38,6 +38,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter  {
         httpSecurity.
                 authorizeRequests()
 
+                .antMatchers("/")
+                .permitAll()
                 .antMatchers("/test")
                 .permitAll()
                 .antMatchers("/ananim/put")
--- src/main/java/com/shoperk/controller/ShopKeepController.java
+++ src/main/java/com/shoperk/controller/ShopKeepController.java
@@ -1,12 +1,16 @@
 package com.shoperk.controller;
 
 import com.shoperk.entity.Shopkeeper;
+import com.shoperk.entity.User;
 import com.shoperk.repository.ShopkeeperRepository;
 import com.shoperk.service.ShopkeeperService;
 import org.hibernate.annotations.common.util.impl.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Date;
 import java.util.List;
@@ -48,7 +52,23 @@ public class ShopKeepController {
 
         return shopkeeperService.save(shopkeeper);
     }
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    @ResponseBody
+    public String list(Authentication authentication){
+        String not_auth="You are not authentication please go to /login page! " +
+                " |||  For user Nasimxon " +
+                "    Login :   Nasim ,   Password : 123456 " +
+                " |||  For user Bakhrom " +
+                "    Login : Achilov ,   Password : 123456";
+        User user=authentication==null?
+                null: (User) authentication.getPrincipal();
+        if(user==null){
+            return not_auth; }
 
+        else
+        return "Welcome " +user.getUserName()+" !";
+
+    }
 
     @RequestMapping(value = "/ananim/put",method = RequestMethod.POST)
     @ResponseBody
